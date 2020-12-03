b7c6211e0a28af7af66ad02fbb9a07a8bac64997
Adam Platt <adamjplatt@outlook.com>
2020-12-03 21:44:28 +0800

    update main, swap header files

--- mp3/DLList.h
+++ mp3/DLList.h
@@ -3,174 +3,142 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
 class DLList {
 
-    struct Node {
-        T x;    
+    struct Node { 
+        int data;
         Node *prev, *next;
     };
     Node dummy;
-    int n;
-    void remove(Node *w);
-    Node* addBefore(Node *w, T x);
-    Node* getNode(int i);
-    Node* addAfter(Node *w, T i);
-
+    int n, d_sum;
+    
     public:
         DLList();
 
-        T get(int i);
-        T set(int i, T x);
-        void add(int i, T x);
-        void add(T x);          // Add to end of DLList.
-        void push(T x);
-        T remove(int i);
-        void print();
-        
-        int size() {return n;};
-};
+        void addAfter(Node *w, int x);
+        Node* getNode(int i);
 
-template <typename T>
-DLList<T>::DLList()
+        void push(int x);
+        void print();
+        int size() {return n;};
+        void sortedInsert(DLList **head_ref, Node* newNode);
+        void insertionSort(DLList **f_elems, DLList **t_elems);
+        void sum();
+        void average();
+}; 
+
+DLList::DLList()
 {
     dummy.next = &dummy;
-    cout << "Dummy next: " << dummy.next << endl;
     dummy.prev = &dummy;
-    cout << "Dummy prev: " << dummy.prev << endl;
-    cout << dummy.next->x << dummy.prev->x << endl;
-    dummy.next->x = dummy.prev->x = NULL;
     n = 0;
 }
 
-template <typename T>
-void DLList<T>::remove(Node *w)
-{
-    w->prev->next = w->next;
-    w->next->prev = w->prev;
-    delete w;
-    n--;
-}
-
-template <typename T>
-typename DLList<T>::Node* DLList<T>::addBefore(Node *w, T x)
+void DLList::addAfter(Node *w, int x)
 {
     Node *u = new Node;
-    u->x = x;
-    u->prev = w->prev;
-    u->next = w;
-    u->next->prev = u;
-    u->prev->next = u;
-    n++;
-    return u;
-}
-
-template <typename T>
-typename DLList<T>::Node* DLList<T>::addAfter(Node *w, T x)
-{
-    cout << endl;
-    cout << endl;
-    cout << "W node = " << w << endl;
-    Node *u = new Node;
-    u->x = x;
-    cout << "U: " << u << endl;
-    cout << "U->X: " << u->x << endl;
+    u->data = x;
     u->next = w->next;
-    cout << "w-next: " << w->next << endl;
-    cout << "u-next: " << u->next << endl;
-    cout << "Should equal first W node: " << w->next->prev << endl;
     w->next->prev = u;
-    cout << "Should equal first W node: " << w->next->prev << endl;
     w->next = u;
-    cout << "w-next: " << w->next << endl;
     u->prev = w;
-    cout << "u-prev: " << u->prev << endl;
     n++;
-    return u;
-    cout << endl;
 }
 
-template <typename T>
-typename DLList<T>::Node* DLList<T>::getNode(int i)
+DLList::Node* DLList::getNode(int i)
 {
-    cout << "I: " << i << endl;
-    Node *p = &dummy;
-    cout << "Dummy: " << p->x << endl;
+    Node *h = &dummy;
     for (int j = 0; j < i; j++){
-        p = p->next;
-        cout << "P: " << p << endl;
+        h = h->next;
     }
-
-
-    /*
-    if (i < n / 2){
-        p = dummy.next;
-        for (int j = 0; j < i; j++)
-            p = p->next;
-    }
-    else{
-        p = &dummy;
-        for (int j = n; j > i; j--)
-            p = p->prev;
-    }
-    */
-    // cout << p << endl;
-    return(p);
+    return(h);
 }
 
-template <typename T>
-T DLList<T>::get(int i)
-{
-    return getNode(i)->x;
-}
-
-template <typename T>
-T DLList<T>::set(int i, T x)
-{
-    Node* u = getNode(i);
-    T y = u->x;
-    u->x = x;
-    return y;
-}
-
-template <typename T>
-void DLList<T>::push(T x)
+void DLList::push(int x)
 {
     addAfter(getNode(size()), x);
 }
 
-
-template <typename T>
-void DLList<T>::add(int i, T x)
+void DLList::print()
 {
-    addBefore(getNode(i), x);
-}
-
-template <typename T>
-void DLList<T>::add(T x)
-{
-    add(size(), x);
-}
-
-template <typename T>
-T DLList<T>::remove(int i)
-{
-    Node *w = getNode(i);
-    T x = w->x;
-    remove(w);
-    return x;
-}
-
-template <typename T>
-void DLList<T>::print()
-{
+    int count = 1;
     Node *h = dummy.next;
-    cout << "H: " << h << endl;
-    Node *t = &dummy;
+    Node *t = h;
 
-    do{
-        cout << h->x << "\n";
+    while(h->next != t){
+        cout << " " << h->data;
         h = h->next;
-    } while (h != t);
+        count++;
+    }
+    cout << "\n";
+}
 
+void DLList::sortedInsert(DLList **sorted, Node* newNode)
+{
+    if((*sorted)->dummy.next == &(*sorted)->dummy){
+        (*sorted)->dummy.next = newNode;
+        (*sorted)->dummy.next->next = &(*sorted)->dummy;
+        (*sorted)->dummy.next->next->prev = (*sorted)->dummy.next;
+    }
+    else if((*sorted)->dummy.next->data == newNode->data)
+        return;    
+    else if((*sorted)->dummy.next->data >= newNode->data){        
+        newNode->next = (*sorted)->dummy.next; 
+        newNode->next->prev = newNode; 
+        (*sorted)->dummy.next = newNode;
+    }
+    else{
+        Node *current = (*sorted)->dummy.next;
+        Node *end = &(*sorted)->dummy;
+        while (current->next != end && newNode->data > current->next->data){
+            if(current->data == newNode->data) cout << "TEST" << endl;
+            current = current->next;
+        }
+        if(current->next->data == newNode->data) return;
+        newNode->next = current->next; 
+        if (current->next != end) 
+            newNode->next->prev = newNode; 
+        current->next = newNode; 
+        newNode->prev = current;
+    }   
+}
+
+void DLList::insertionSort(DLList **f_elems, DLList **t_elems)
+{
+    // Combine two LLists; single traversal for insertion sort.
+    (*f_elems)->dummy.prev->next = (*t_elems)->dummy.next;
+    (*t_elems)->dummy.prev->next = &(*f_elems)->dummy;
+    
+    DLList *sorted = new DLList;
+    Node *current = (*f_elems)->dummy.next;
+    Node *finish = &(*f_elems)->dummy;
+
+    while(current != finish){
+        Node *next = current->next;
+        current->prev = current->next = NULL;
+        sortedInsert(&sorted, current);
+        current = next;
+    }
+    *this = *sorted;
+}
+
+void DLList::sum()
+{
+    int count = 0;
+    d_sum = 1;
+    Node *h = dummy.next;
+    Node *t = h;
+
+    while(h->next != t){
+        d_sum += h->data;
+        count++;
+        h = h->next;
+    }
+    n = count;
+    cout << "The sum of the merged DLLists are: " << d_sum << "\n";
+}
+
+void DLList::average()
+{
+    cout << "The average of the merged DLLists are: " << d_sum / n << endl;
 }
--- mp3/mp3-main.cpp
+++ mp3/mp3-main.cpp
@@ -1,14 +1,32 @@
 #include "DLList.h"
 #include <iostream>
+#include <cmath>
 
-using namespace std;
+int MAX_ENTRIES_15 = 15;
+int MAX_ENTRIES_20 = 20;
 
 int main ()
 {
-    DLList<int> dllist;
+    DLList *fifteen_elems = new DLList;
+    DLList *twenty_elems = new DLList;
+    DLList *sorted = new DLList;
 
-    dllist.push(1);
-    dllist.push(3);
-    dllist.push(5);
-    dllist.print();
+    int entry_count = 0;
+
+    while(entry_count < MAX_ENTRIES_20){
+        int rand_one = rand() % 100 + 0;
+        twenty_elems->push(rand_one);
+        if(entry_count < MAX_ENTRIES_15){
+            int rand_two = rand() % 100 + 0;
+            fifteen_elems->push(rand_two);
+        }
+        entry_count++;
+    }
+    cout << "Linked List MP3 by Adam Platt" << "\n";
+    cout << "DLList with 15 Elements:"; fifteen_elems->print();
+    cout << "DLList with 20 Elements:"; twenty_elems->print();
+    sorted->insertionSort(&fifteen_elems, &twenty_elems);
+    cout << "Merged DLList:"; sorted->print();
+    sorted->sum();
+    sorted->average();
 }
